d2f521ccfeceacb8f22f73af0417cb346a3120be
unknown <ruben.fabio2016@gmail.com>
2022-04-30 16:12:36 -0300

    filtro de pokemons

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,20 +6,28 @@ import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
 
 import Layout from '../components/Layout';
 
-const tipo = [
-  { name: 'Selecione um tipo' },
-  { name: 'Fire' },
-  { name: 'Grass' },
-  { name: 'Poison' },
-  { name: 'Flying' },
-  { name: 'Water' },
-  { name: 'Bug' },
-];
+export default function Home({ arrayPokemon2, tipos }) {
+  const [selected, setSelected] = useState('Selecione o Pokemon');
 
-export default function Home({ arrayPokemon2 }) {
-  const [selected, setSelected] = useState(tipo[0]);
+  const [pokemons, setPokemons] = useState(arrayPokemon2);
 
-  console.log('arrayPokemon', arrayPokemon2);
+  function getPokemons(ostipos) {
+    setPokemons(arrayPokemon2);
+    if (ostipos === 'limpar') {
+      setPokemons(arrayPokemon2);
+    } else {
+      let pokemonfiltrado = arrayPokemon2
+        .filter((pokemon) =>
+          pokemon.types.some((tipo) => tipo.type.name === ostipos)
+        )
+        .map((item2) => {
+          let pokemonTem = { ...item2 };
+          return pokemonTem;
+        });
+      setPokemons(pokemonfiltrado);
+    }
+  }
+
   return (
     <Layout title="Pokedex">
       <section className="px-10">
@@ -32,7 +40,7 @@ export default function Home({ arrayPokemon2 }) {
             >
               <div className="relative mt-1">
                 <Listbox.Button className="focus:outline-none relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                  <span className="block truncate">{selected.name}</span>
+                  <span className="block truncate capitalize">{selected}</span>
                   <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                     <SelectorIcon
                       className="h-5 w-5 text-gray-400"
@@ -47,24 +55,25 @@ export default function Home({ arrayPokemon2 }) {
                   leaveTo="opacity-0"
                 >
                   <Listbox.Options className="max-w-sm focus:outline-none absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 sm:text-sm z-20">
-                    {tipo.map((types, typeIdx) => (
+                    {tipos.results.map((types, typeIdx) => (
                       <Listbox.Option
                         key={typeIdx}
                         className={({ active }) =>
-                          `relative cursor-default select-none py-2 pl-10 pr-4 z-20 ${
+                          `relative cursor-default select-none py-2 pl-10 pr-4 z-20 capitalize ${
                             active
                               ? 'bg-amber-100 text-amber-900'
                               : 'text-gray-900'
                           }`
                         }
-                        value={types}
+                        value={types.name}
                       >
                         {({ selected }) => (
                           <>
                             <span
-                              className={`block truncate ${
+                              className={`block truncate capitalize ${
                                 selected ? 'font-medium' : 'font-normal'
                               }`}
+                              onClick={() => getPokemons(types.name)}
                             >
                               {types.name}
                             </span>
@@ -86,7 +95,7 @@ export default function Home({ arrayPokemon2 }) {
             </Listbox>
           </div>
           <div className="px-32 py-20 grid grid-cols-3 gap-[55px]">
-            {arrayPokemon2.map((pokemon, index) => (
+            {pokemons.map((pokemon, index) => (
               <Link
                 href={{
                   pathname: `pokemon/${pokemon.name}`,
@@ -128,6 +137,9 @@ export default function Home({ arrayPokemon2 }) {
 }
 
 export async function getServerSideProps() {
+  const resTipos = await fetch('https://pokeapi.co/api/v2/type');
+  const tipos = await resTipos.json();
+
   const trazerPokemon = (numero) => {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`)
       .then((response) => response.json())
@@ -135,7 +147,7 @@ export async function getServerSideProps() {
   };
   let arrayPokemon = [];
 
-  for (let index = 1; index <= 20; index++) {
+  for (let index = 1; index <= 50; index++) {
     let data = await trazerPokemon(index);
     arrayPokemon.push(data);
   }
@@ -150,6 +162,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      tipos: tipos,
       arrayPokemon2,
     },
   };
